2d87ea6a277f7329137b57892817c8b6712ef7be
Jonah Abraham <jonah.abraham@slalom.com>
2022-12-01 17:18:51 -0500

    first commit

---
+++ ad1.py
@@ -0,0 +1,49 @@
+from calendar import THURSDAY
+
+
+bigList = []
+littleList = []
+
+with open('input.txt') as f:
+    lines = f.readlines()
+    
+    for line in lines:
+
+        bigList.append(line.strip())
+
+firstPlace = 0
+secondPlace = 0
+thirdPlace = 0
+
+runningTotal = 0
+
+
+for item in bigList:
+    if item != "":
+        runningTotal = int(item) + runningTotal
+    if item == "":
+        
+        #print(runningTotal)
+
+        if runningTotal >= firstPlace:
+            thirdPlace = secondPlace
+            secondPlace = firstPlace
+            firstPlace = runningTotal
+            runningTotal = 0
+        if runningTotal >= secondPlace and runningTotal < firstPlace:
+            thirdPlace = secondPlace
+            secondPlace = runningTotal
+            runningTotal = 0
+        if runningTotal >= thirdPlace and runningTotal < secondPlace:
+            thirdPlace = runningTotal
+            runningTotal = 0
+        if runningTotal < thirdPlace:
+            runningTotal = 0
+
+
+
+print(firstPlace)
+print(secondPlace)
+print(thirdPlace)
+
+print(firstPlace+secondPlace+thirdPlace)
